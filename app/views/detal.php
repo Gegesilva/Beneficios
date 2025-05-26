3d3871651f4f7320d7806b97aa903fbbed560cfc
Gegesilva <103137242+Gegesilva@users.noreply.github.com>
2025-05-26 15:51:51 -0300

    26-05

--- app/views/detal.php
+++ app/views/detal.php
@@ -47,10 +47,70 @@ switch ($tipoValor) {
                 ";
         break;
     case $tipoValor == 'U':
-        $sql = "";
+        $sql = "SELECT
+                        tb02278_codigo as CODIGO,
+                        TB02278_SITUACAO AS SITUACAO,
+                        TB02278_CINTERNO AS CODIGO_INTERNO,
+                        TB02278_NUMVENDA as VENDA_ORIGEM_BENEFICIO,
+                        NTFISC NUM_NOTA,
+                        FORMAT(ISNULL (TB02091_DATANOTA, TB02278_DATA), 'dd/MM/yyyy')  AS DATA_NOTA,
+                        FORMAT(TB02278_DATA, 'dd/MM/yyyy') AS DATA_BENEFICIO,
+                        TB01074_NOME AS TIPO_BENEFICIO,
+                        TB02278_NOME AS HISTORICO,
+                        TB02278_CODCLI AS CODCLI,
+                        A.TB01008_NOME AS CLIENTE,
+                        TB02278_MES AS MES,
+                        FORMAT(TB02278_DTVALIDADE, 'dd/MM/yyyy') AS VALIDADE,
+                        TB02278_VLRBENEF AS VALOR_BENEFICIO,
+                        TB02278_VLRUTILIZADO AS VALOR_UTILIZADO,
+                        TB02278_VLRREST AS VALOR_RESTANTE,
+                        TB02278_MARCANOME AS MARCA,
+                        TB01107_NOME AS GRUPO_ECONOMICO
+
+
+        from VW02311
+
+        LEFT JOIN TB02278 ON TB02278_CODIGO = BENEFICIO
+        LEFT JOIN TB01008 AS A ON TB01008_CODIGO = TB02278_CODCLI
+        LEFT JOIN TB01107 ON TB01107_CODIGO = TB01008_GRUPO
+        LEFT JOIN TB01074 ON TB01074_CODIGO = TB02278_CLASSIFICACAO
+        LEFT JOIN TB02091 ON TB02091_NTFISC = NTFISC AND TB02091_CODEMP = CODEMP
+
+        WHERE CODCLI = ?
+        AND CAST(TB02091_DATA AS DATE) BETWEEN ? AND ?";
         break;
     case $tipoValor == 'E':
-        $sql = "";
+        $sql = "SELECT
+                    VW02310.tb02278_codigo as CODIGO,
+                    VW02310.TB02278_SITUACAO AS SITUACAO,
+                    VW02310.TB02278_CINTERNO AS CODIGO_INTERNO,
+                    VW02310.TB02278_NUMVENDA as VENDA_ORIGEM_BENEFICIO,
+                    TB02021_NTFISC NUM_NOTA,
+                    FORMAT(ISNULL (TB02091_DATANOTA, VW02310.TB02278_DATA), 'dd/MM/yyyy')  AS DATA_NOTA,
+                    FORMAT(VW02310.TB02278_DATA, 'dd/MM/yyyy') AS DATA_BENEFICIO,
+                    TB01074_NOME AS TIPO_BENEFICIO,
+                    VW02310.TB02278_NOME AS HISTORICO,
+                    VW02310.TB02278_CODCLI AS CODCLI,
+                    A.TB01008_NOME AS CLIENTE,
+                    VW02310.TB02278_MES AS MES,
+                    FORMAT(VW02310.TB02278_DTVALIDADE, 'dd/MM/yyyy') AS VALIDADE,
+                    VW02310.TB02278_VLRBENEF AS VALOR_BENEFICIO,
+                    VW02310.TB02278_VLRUTILIZADO AS VALOR_UTILIZADO,
+                    VW02310.TB02278_VLRREST AS VALOR_RESTANTE,
+                    VW02310.TB02278_MARCANOME AS MARCA,
+                    TB01107_NOME AS GRUPO_ECONOMICO
+
+            from VW02310
+
+            LEFT JOIN TB02278 AS B ON B.TB02278_CODIGO = vw02310.TB02278_CODIGO
+            LEFT JOIN TB01008 AS A ON TB01008_CODIGO = vw02310.TB02278_CODCLI
+            LEFT JOIN TB01107 ON TB01107_CODIGO = A.TB01008_GRUPO
+            LEFT JOIN TB02021 ON TB02021_CODIGO = vw02310.TB02278_NUMVENDA
+            LEFT JOIN TB02091 ON TB02091_NTFISC = TB02021_NTFISC
+
+            WHERE vw02310.TB02278_CODCLI = ?
+            AND CAST(DATAMES AS DATE) BETWEEN ? AND ?
+            AND vw02310.TB02278_SITUACAO = 'I'";
         break;
 }
 
@@ -67,7 +127,7 @@ $nomesMeses = [
     10 => 'outubro',
     11 => 'novembro',
     12 => 'dezembro'
-  ];
+];
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -102,24 +162,35 @@ $nomesMeses = [
                                 aria-hidden="true"></i></th>
                         <th class="titulo-col-tab" onclick="ordenarTabela(6)">Data Beneficio <i class="fa fa-sort"
                                 aria-hidden="true"></i></th>
-                        <th class="titulo-col-tab" onclick="ordenarTabela(7)">Historico <i class="fa fa-sort"
+                        <th class="titulo-col-tab" onclick="ordenarTabela(7)">Tipo Beneficio <i class="fa fa-sort"
                                 aria-hidden="true"></i></th>
-                        <th class="titulo-col-tab" onclick="ordenarTabela(8)">Cliente <i class="fa fa-sort"
+                        <th class="titulo-col-tab" onclick="ordenarTabela(8)">Historico <i class="fa fa-sort"
                                 aria-hidden="true"></i></th>
-                        <th class="titulo-col-tab" onclick="ordenarTabela(9)">Mês <i class="fa fa-sort"
+                        <th class="titulo-col-tab" onclick="ordenarTabela(9)">Cliente <i class="fa fa-sort"
                                 aria-hidden="true"></i></th>
-                        <th class="titulo-col-tab" onclick="ordenarTabela(10)">Validade <i class="fa fa-sort"
+                        <th class="titulo-col-tab" onclick="ordenarTabela(10)">Mês <i class="fa fa-sort"
+                                aria-hidden="true"></i></th>
+                        <th class="titulo-col-tab" onclick="ordenarTabela(11)">Validade <i class="fa fa-sort"
+                                aria-hidden="true"></i></th>
+                        <th class="titulo-col-tab" onclick="ordenarTabela(12)">Valor Beneficio <i class="fa fa-sort"
+                                aria-hidden="true"></i></th>
+                        <th class="titulo-col-tab" onclick="ordenarTabela(13)">Valor Restante <i class="fa fa-sort"
+                                aria-hidden="true"></i></th>
+                        <th class="titulo-col-tab" onclick="ordenarTabela(14)">Marca <i class="fa fa-sort"
+                                aria-hidden="true"></i></th>
+                        <th class="titulo-col-tab" onclick="ordenarTabela(15)">Grupo <i class="fa fa-sort"
                                 aria-hidden="true"></i></th>
                         <th>
+
                             <button class="btn-xls-detal" onclick="exportarExcel()"></button>
                             <button class="btn-voltar-detal" onclick="voltar()"></button>
                         </th>
                     </tr>
                 </thead>
                 <?php
-                
 
-                $stmt = sqlsrv_prepare($conn, $sql, [$cliente, $dataIni, $dataFim]);    
+
+                $stmt = sqlsrv_prepare($conn, $sql, [$cliente, $dataIni, $dataFim]);
                 sqlsrv_execute($stmt);
                 if ($stmt === false) {
                     die(print_r(sqlsrv_errors(), true));
@@ -141,12 +212,13 @@ $nomesMeses = [
                         $vlrRest = (float) $row['VALOR_RESTANTE'];
 
                         $totalBeneficio += $vlrBeneficio;
-                        $totalRest += $vlrRest; 
+                        $totalRest += $vlrRest;
 
                         $tabela .= "<tr>";
                         $tabela .= "<td>$row[CODIGO]</td>";
                         $tabela .= "<td>$row[SITUACAO]</td>";
                         $tabela .= "<td>$row[CODIGO_INTERNO]</td>";
+                        $tabela .= "<td>$row[VENDA_ORIGEM_BENEFICIO]</td>";
                         $tabela .= "<td>$row[NUM_NOTA]</td>";
                         $tabela .= "<td>$row[DATA_NOTA]</td>";
                         $tabela .= "<td>$row[DATA_BENEFICIO]</td>";
@@ -168,9 +240,11 @@ $nomesMeses = [
                 </tbody>
                 <tfoot>
                     <tr style="font-size: 0.75rem; font-weight: normal; color: #555;">
-                        <th colspan="8" style="text-align: right;">Totais:</th>
+                        <th colspan="12" style="text-align: right;">Totais:</th>
                         <th><?= formatarMoeda($totalBeneficio) ?></th>
                         <th><?= formatarMoeda($totalRest) ?></th>
+                        <th></th>
+                        <th></th>
                         <th></th>
                     </tr>
                 </tfoot>
